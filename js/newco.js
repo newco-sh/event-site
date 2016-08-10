3d0c19305959dd334e15c00176f7f03d5daf414c
Leo Li <runzhou.li@gmail.com>
2016-08-10 08:26:43 +0800

    updated hosts

--- js/newco.js
+++ js/newco.js
@@ -1,23 +1,43 @@
 /* DATA */
-var _hosts = {
-  'Robin8': {
-    name: 'Robin8',
-    logo: 'img/hosts/Robin8.png',
-    presenter: 'Miranda Tan',
-    avatar: 'img/hosts/Robin8_Miranda Tan.png',
+var _hosts = [
+  {
+    k: 'ChinaNetCloud',
+    name: 'ChinaNetCloud云络科技',
+    logo: 'img/hosts/ChinaNetCloud.png',
+    presenter: 'Steve Mushero',
+    avatar: 'img/hosts/ChinaNetCloud_Steve Mushero.jpg',
     lang: 'EN',
+    position: 'CEO',
+    industry: 'Cloud Services',
+    p_intro: {
+      en: '<p>Mr. Steve Mushero co-founded ChinaNetCloud in 2008 and serves as its Chief Technology Officer and Chief Executive Officer. Mr. Mushero served as the Chief Technical Officer of Tudou Holdings Limited. He has over 20 years of technology management experience across a wide range of industries and international contexts. </p>',
+      zh: '<p>Steve Mushero先生在2008年共同创立了云络科技，担任公司的CTO和CEO。他曾在硅谷担任过各种网络技术创业公司的CTO，后来成为土豆网的第一名外籍CTO。</p>'
+    },
+    intro: {
+      en: "<p>The world's largest Internet server management company, and China's oldest Cloud Computing business.<p>",
+      zh: '<p>全球最大的互联网服务器管理公司、中国最早提供服务器管理及云计算服务的公司。</p>'
+    }
+  },
+  {
+    k: 'FotoPlace',
+    name: 'FotoPlace 足记',
+    logo: 'img/hosts/FotoPlace足记.png',
+    presenter: '杨柳/Yang Liu',
+    avatar: 'img/hosts/FotoPlace_Yang Liu.jpg',
+    lang: 'CN',
     position: 'CEO',
     industry: 'Social App',
     p_intro: {
-      en: '<p>Miranda Tan has more than 10 years experience in public relations, marketing, and tech. Miranda has a BS from Cornell University and a JD from St. John’s University School of Law.</p>',
-      zh: ''
+      en: '<p>Yang Liu, born in 1970s, graduated from Shanghai Normal University with a degree in Dramatic Literature. In love with films, her job for over a decade was instead in product management. In 2014, she finally merged her love for films and her experience in product management, leading to the inception of FotoPlace. </p>',
+      zh: '<p>杨柳，70后白羊座，风风火火恍恍惚惚。。上海师范大学影视戏剧文学专业毕业。作为一个长期电影中毒者，十几年工龄却没有从事与电影相关的工作。2014年，人到中年，无心插柳意外开始创业。当初喜欢的专业与十来年的工作经历终于殊途同归，可见一切自有妥帖的安排。作为「足记」APP的创始人，也是「足记」最大的“bug”（据旁人追忆，此语出自某次醉生梦死后）。非典型互联网产品汪，女汉砸，记性不好，智商堪忧，懒散，好吃，代谢慢，不运动，胖，不好看，心大，学渣一枚。闪闪发亮的深井冰。一个看起来奔放，但本质上内向的人，尴尬症晚期患者，不擅应对各类冷场。</p>'
     },
     intro: {
-      en: "<p>ROBIN8 makes reaching out to reporters and influencers effortless and effective. We're changing the face of PR with technology that's been years in the making.</p>",
-      zh: ''
+      en: '<p>An image-based social App using movie language to record life. Launched in December 2014, FotoPlace had taken China’s social media world in early 2015, rocketing to the top of photo-type app rankings across several countries and totaling 30m users. </p>',
+      zh: '<p>足记是一款用电影语言记录生活的图片社交应用。通过足记，可以轻松将普通照片制作成充满电影感的图片，将平凡生活记录成电影，做自己生活的导演。同时，在足记中还可以查找电影取景地，跟随电影足迹、名人与历史的足迹发现身边的美。足记在2014年12月上线正式版本，2015年2月初增加了大片模式，随后在朋友圈引爆了3000万用户的电影情怀，横扫几十个国家的摄影排行榜榜首。</p>'
     }
   },
-  'Italki': {
+  {
+    k: 'Italki',
     name: 'Italki',
     logo: 'img/hosts/italki.gif',
     presenter: 'Kevin Chen',
@@ -27,48 +47,33 @@ var _hosts = {
     industry: 'EduTech',
     p_intro: {
       en: '<p>Kevin Chen is co-founder of italki.com. Previously, he co-founded famento.com, and worked in financial research. He is active in the Shanghai startup community through techyizu.org and xinchejian.com.</p>',
-      zh: ''
+      zh: '<p>Kevin Chen是italki的联合创始人，此前他曾从事金融业。Kevin活跃于上海创业圈，如非营利性极客交流社区techyizu以及创客空间新车间。</p>'
     },
     intro: {
       en: '<p>On italki you can find the best online language teachers from around the world. italki is the world leader in online language education with over 1.2 million students and 1500+ teachers of 100 languages.</p>',
-      zh: ''
+      zh: '<p>italki是全球知名的一对一在线外教平台，拥有三千多名活跃的注册教师，每天教授数以千计的在线付费课程。</p>'
     }
   },
-  'ChinaNetCloud': {
-    name: 'ChinaNetCloud',
-    logo: 'img/hosts/ChinaNetCloud.png',
-    presenter: 'Steve Mushero',
-    avatar: 'img/hosts/ChinaNetCloud_Steve Mushero.jpg',
+  {
+    k: 'Robin8',
+    name: 'Robin8',
+    logo: 'img/hosts/Robin8.png',
+    presenter: 'Miranda Tan',
+    avatar: 'img/hosts/Robin8_Miranda Tan.png',
     lang: 'EN',
-    position: 'CEO',
-    industry: 'Cloud Services',
-    p_intro: {
-      en: '<p>Mr. Steve Mushero co-founded ChinaNetCloud in 2008 and serves as its Chief Technology Officer and Chief Executive Officer. Mr. Mushero served as the Chief Technical Officer of Tudou Holdings Limited. He has over 20 years of technology management experience across a wide range of industries and international contexts. </p>',
-      zh: ''
-    },
-    intro: {
-      en: "<p>The world's largest Internet server management company, and China's oldest Cloud Computing business.<p>",
-      zh: ''
-    }
-  },
-  'FotoPlace': {
-    name: 'FotoPlace 足记',
-    logo: 'img/hosts/FotoPlace足记.png',
-    presenter: '杨柳/Yang Liu',
-    avatar: 'img/hosts/FotoPlace_Yang Liu.jpg',
-    lang: 'CN',
     position: 'CEO',
     industry: 'Social App',
     p_intro: {
-      en: '',
-      zh: '<p>杨柳，70后白羊座，风风火火恍恍惚惚。。上海师范大学影视戏剧文学专业毕业。作为一个长期电影中毒者，十几年工龄却没有从事与电影相关的工作。2014年，人到中年，无心插柳意外开始创业。当初喜欢的专业与十来年的工作经历终于殊途同归，可见一切自有妥帖的安排。作为「足记」APP的创始人，也是「足记」最大的“bug”（据旁人追忆，此语出自某次醉生梦死后）。非典型互联网产品汪，女汉砸，记性不好，智商堪忧，懒散，好吃，代谢慢，不运动，胖，不好看，心大，学渣一枚。闪闪发亮的深井冰。一个看起来奔放，但本质上内向的人，尴尬症晚期患者，不擅应对各类冷场。</p>'
+      en: '<p>Miranda Tan has more than 10 years experience in public relations, marketing, and tech. Miranda has a BS from Cornell University and a JD from St. John’s University School of Law.</p>',
+      zh: '<p>Miranda Tan 毕业于康奈尔大学, 在PR、市场营销和公共关系领域有超过15年的经验。</p>'
     },
     intro: {
-      en: '<p>An image-based social App using movie language to record life.</p>',
-      zh: '<p>足记是一款用电影语言记录生活的图片社交应用。通过足记，可以轻松将普通照片制作成充满电影感的图片，将平凡生活记录成电影，做自己生活的导演。同时，在足记中还可以查找电影取景地，跟随电影足迹、名人与历史的足迹发现身边的美。足记在2014年12月上线正式版本，2015年2月初增加了大片模式，随后在朋友圈引爆了3000万用户的电影情怀，横扫几十个国家的摄影排行榜榜首。</p>'
+      en: "<p>ROBIN8 makes reaching out to reporters and influencers effortless and effective. We're changing the face of PR with technology that's been years in the making.</p>",
+      zh: '<p>Robin8是基于大数据的网红和新媒体广告平台。Robin8的NLP系统可以帮助品牌找到最合适的网红和新媒体。ROBIN8的投资人包括线性资本，Pre Angel，SOSV, 华睿互联，凯洲资本及365投资等 。Robin8目前在纽约、柏林、上海均设有办公室。</p>'
     }
   },
-  'Teambition': {
+  {
+    k: 'Teambition',
     name: 'Teambition',
     logo: 'img/hosts/Teambition.png',
     presenter: 'Yaqian Wang/王雅倩',
@@ -81,102 +86,105 @@ var _hosts = {
       zh: '<p>上海交通大学管理学学士，应用心理学硕士，瑞典隆德大学管理学博士。博士期间在欧洲最大的包装公司 SCA Packaging 三个欧洲分公司进行组织创新文化的研究，并曾在麦肯锡公司上海分公司，意大利水泥集团米兰总部工作实习。2014年博士毕业回国后加入 Teambition，负责公司内部运营，带领 Teambition 从20多个人成长为100+人。</p>'
     },
     intro: {
-      en: '',
-      zh: ''
+      en: "<p>China's leading workplace collaboration app. In 2015, it raised $12 million in Series B funding.</p>",
+      zh: '<p>国内领先的团队协作工具，2015年完成1200万美元的B轮融资。</p>'
     }
   }
-};
-var _agenda = [{
-  date: '2016-09-12',
-  slots: [{
-    start: '19:00',
-    end: '22:00',
-    events: [{
-      en: 'VIP Dinner',
-      zh: 'VIP 启动晚宴'
+];
+var _agenda = [
+  {
+    date: '2016-09-12',
+    slots: [{
+      start: '19:00',
+      end: '22:00',
+      events: [{
+        en: 'VIP Dinner',
+        zh: 'VIP 启动晚宴'
+      }]
     }]
-  }]
-}, {
-  date: '2016-09-13',
-  slots: [{
-    start: '09:00',
-    end: '10:00',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
+  },
+  {
+    date: '2016-09-13',
+    slots: [{
+      start: '09:00',
+      end: '10:00',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '10:00',
+      end: '11:00',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '11:00',
+      end: '12:00',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '12:00',
+      end: '13:30',
+      events: [{
+        en: 'Lunch Break',
+        zh: '午休'
+      }]
+    }, {
+      start: '13:30',
+      end: '14:30',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '14:30',
+      end: '15:30',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '15:30',
+      end: '16:30',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '16:30',
+      end: '17:30',
+      events: [{
+        en: 'TBD',
+        zh: '待定'
+      }]
+    }, {
+      start: '17:30',
+      end: '19:00',
+      events: [{
+        en: 'Dinner Break',
+        zh: '晚休'
+      }]
+    }, {
+      start: '19:00',
+      end: '21:00',
+      events: [{
+        en: '8x8 Speaking Event',
+        zh: '8x8企业家分享'
+      }]
+    }, {
+      start: '21:00',
+      end: '',
+      events: [{
+        en: 'After Party',
+        zh: 'After Party'
+      }]
     }]
-  }, {
-    start: '10:00',
-    end: '11:00',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '11:00',
-    end: '12:00',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '12:00',
-    end: '13:30',
-    events: [{
-      en: 'Lunch Break',
-      zh: '午休'
-    }]
-  }, {
-    start: '13:30',
-    end: '14:30',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '14:30',
-    end: '15:30',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '15:30',
-    end: '16:30',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '16:30',
-    end: '17:30',
-    events: [{
-      en: 'TBD',
-      zh: '待定'
-    }]
-  }, {
-    start: '17:30',
-    end: '19:00',
-    events: [{
-      en: 'Dinner Break',
-      zh: '晚休'
-    }]
-  }, {
-    start: '19:00',
-    end: '21:00',
-    events: [{
-      en: '8x8 Speaking Event',
-      zh: '8x8企业家分享'
-    }]
-  }, {
-    start: '21:00',
-    end: '',
-    events: [{
-      en: 'After Party',
-      zh: 'After Party'
-    }]
-  }]
-}];
+  }
+];
 /* don't boil me */
 $(function() {
   $('#full-page').fullpage({
@@ -217,7 +225,10 @@ $(function() {
     },
     computed: {
       host: function() {
-        return this.hosts[this.selected_host];
+        var self = this;
+        return self.hosts.find(function(host) {
+          return host.k === self.selected_host;
+        });
       }
     },
     methods: {
